5a8dd4b8aa55dafeb559833aa2b65a9d791e8f4c
GreyJolly <vortex.minecraft@libero.it>
2024-05-28 15:01:19 +0200

    Implemented users

--- PriceComparer/config/application.rb
+++ PriceComparer/config/application.rb
@@ -18,5 +18,11 @@ module PriceComparer
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    # config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework = false
+    end
   end
 end
