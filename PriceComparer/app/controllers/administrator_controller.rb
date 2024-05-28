e1bd0fce748527c3e9c7eb51072602c00ff68e10
GreyJolly <vortex.minecraft@libero.it>
2024-05-28 18:58:32 +0200

    Implemented togglable roles

--- PriceComparer/app/controllers/administrator_controller.rb
+++ PriceComparer/app/controllers/administrator_controller.rb
@@ -1,6 +1,34 @@
 class AdministratorController < ApplicationController
+	before_action :authorize_admins
 
-def users_list
-    @users = User.all
+	def authorize_admins
+		unless current_user && current_user.isAdministrator
+		  flash[:alert] = "You are not authorized to access the users list. You have been redirected"
+		  redirect_to root_path # Or any other path
+		end
+	  end
+
+	def users_list
+    	@users = User.all
+	end
+
+	def toggle_analyst
+    	@user = User.find(params[:id])
+    	@user.update(isAnalyst: !@user.isAnalyst)
+    	redirect_back(fallback_location: root_path)
+	end
+
+	def toggle_admin
+    	@user = User.find(params[:id])
+    	@user.update(isAdministrator: !@user.isAdministrator)
+    	redirect_back(fallback_location: root_path)
+	end
+
+	  def authorize_specific_user
+    specific_user_id = 123 # Replace with the ID of the specific user
+    unless current_user && current_user.id == specific_user_id
+      flash[:alert] = "You are not authorized to access this page."
+      redirect_to root_path # Or any other path
+    end
   end
 end
