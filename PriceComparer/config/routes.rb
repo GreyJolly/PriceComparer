1be07936d3d5c2fc5b961553b5de9f277f8ee775
GreyJolly <vortex.minecraft@libero.it>
2024-05-28 18:58:32 +0200

    Implemented togglable roles

--- PriceComparer/config/routes.rb
+++ PriceComparer/config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 	# Devise routes with OmniAuth callbacks
 	devise_for :users, controllers: { sessions: 'sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
 	
-	
-
 	# Resource routes for products with custom member actions
 	resources :products do
 	  member do
@@ -11,6 +9,15 @@ Rails.application.routes.draw do
 		patch 'report', to: 'home#report'
 	  end
 	end
+	
+	# Resource routes for users with custom member actions
+	resources :users do
+		member do
+		  patch 'toggle_analyst', to: 'administrator#toggle_analyst'
+		  patch 'toggle_admin', to: 'administrator#toggle_admin'
+		end
+	  end
+	
   
 	# Custom route for the administrator user list
 	get 'users_list', to: 'administrator#users_list'
