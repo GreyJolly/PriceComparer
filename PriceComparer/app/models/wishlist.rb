031d19d345edd19c116d50a50f2bf5eb8d895614
Elisabetta115 <erusso1502@gmail.com>
2024-06-03 17:13:40 +0200

    Fixed Wishlist model

--- PriceComparer/app/models/wishlist.rb
+++ PriceComparer/app/models/wishlist.rb
@@ -1,7 +1,4 @@
 class Wishlist < ApplicationRecord
-<<<<<<< HEAD
-=======
 	belongs_to :product
 	belongs_to :user, foreign_key: 'username', primary_key: 'username'
->>>>>>> 0ec63ad6fdf6d1bdd187b16eef7ca984a0512c73
 end
