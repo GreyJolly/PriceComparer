7128ab88f3a04d24443dc5d68ac070ec475bd3f7
GreyJolly <vortex.minecraft@libero.it>
2024-06-03 17:03:09 +0200

    Fixed merge

--- PriceComparer/config/routes.rb
+++ PriceComparer/config/routes.rb
@@ -1,44 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-	# Devise routes with OmniAuth callbacks
-	devise_for :users, controllers: { sessions: 'sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
-	
-	# Resource routes for products with custom member actions
-	resources :products do
-		collection do
-			get 'search', to: 'home#search'
-		end
-	  member do
-		patch 'wishlist', to: 'home#wishlist'
-		patch 'report', to: 'home#report'
-	  end
-	end
-
-	
-	# Resource routes for users with custom member actions
-	resources :users do
-
-		collection do
-			get 'search_users', to: 'administrator#search_users'
-		end
-
-		member do
-		  patch 'toggle_analyst', to: 'administrator#toggle_analyst'
-		  patch 'toggle_admin', to: 'administrator#toggle_admin'
-		end
-	end
-
-
-	
-  
-	# Custom route for the administrator user list
-	get 'users_list', to: 'administrator#users_list'
-  
-	# Root path
-	root to: 'home#index'
-  end
-  
-=======
   # Devise routes with OmniAuth callbacks
   devise_for :users, controllers: { sessions: "sessions", omniauth_callbacks: "users/omniauth_callbacks" }
 
@@ -84,4 +44,3 @@ Rails.application.routes.draw do
   # Root path
   root to: "home#index"
 end
->>>>>>> 0ec63ad6fdf6d1bdd187b16eef7ca984a0512c73
