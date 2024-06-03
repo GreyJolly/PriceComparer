d00b7f92d87e3f594fdc6fdaecb8393c1331dd5f
Elisabetta115 <erusso1502@gmail.com>
2024-06-03 16:49:46 +0200

    Implemented eBay search

--- PriceComparer/app/controllers/home_controller.rb
+++ PriceComparer/app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-
   def wishlist
     @product = Product.find(params[:id])
     # Logic to wishlist
@@ -20,16 +19,53 @@ class HomeController < ApplicationController
     @query = params[:query]
     @min_price = params[:min_price]
     @max_price = params[:max_price]
-    @products = if @query.present?
-                  Product.where("name LIKE ? OR description LIKE ?", "%#{@query}%", "%#{@query}%")
-                else
-                  Product.all
-                end
-    @products = @products.where("price >= ?", @min_price) if @min_price.present?
-    @products = @products.where("price <= ?", @max_price) if @max_price.present?
-    @products = @products.order(price: :asc) if params[:order] == 'asc'
-    @products = @products.order(price: :desc) if params[:order] == 'desc'
+    @order = params[:order]
+  
+    # Fetch eBay items
+    @ebay_items = if @query.present?
+                    EbayService.search_items(@query)
+                  else
+                    []
+                  end
+  
+    Rails.logger.info "Ebay items: #{@ebay_items}"
+  
+    @ebay_items ||= []  # Ensure @ebay_items is an array
+  
+    # Map eBay items to OpenStruct for uniformity
+    ebay_products = @ebay_items.map do |item|
+      OpenStruct.new(
+        name: item['title'][0],
+        description: item['title'][0],  # Assuming eBay items don't have a separate description field
+        site: 'eBay',
+        price: item.dig('sellingStatus', 0, 'currentPrice', 0, '__value__').to_f,  # Convert price to float for comparison
+        currency: item.dig('sellingStatus', 0, 'currentPrice', 0, '@currencyId'),
+        url: item['viewItemURL'][0]
+      )
+    end
+  
+    Rails.logger.info "Mapped eBay products: #{ebay_products}"
+  
+    # Fetch and filter database products
+    @products = Product.all
+    @products = @products.where("name LIKE ? OR description LIKE ?", "%#{@query}%", "%#{@query}%") if @query.present?
+    @products = @products.where("price >= ?", @min_price.to_f) if @min_price.present?
+    @products = @products.where("price <= ?", @max_price.to_f) if @max_price.present?
+  
+    Rails.logger.info "Filtered database products: #{@products}"
+  
+    # Combine and sort products
+    @combined_products = (@products + ebay_products)
+    @combined_products = if @order == 'asc'
+                           @combined_products.sort_by { |product| product.price }
+                         elsif @order == 'desc'
+                           @combined_products.sort_by { |product| -product.price }
+                         else
+                           @combined_products
+                         end
+  
+    Rails.logger.info "Combined products: #{@combined_products}"
+  
     render :index
   end
-
 end
