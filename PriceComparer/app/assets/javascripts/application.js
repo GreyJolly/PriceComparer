04464c23f99572de571804bb3869aa7adc562125
frastricker <francesco.maura@engine4you.it>
2024-06-07 17:29:16 +0200

    material design
    first test with material design

---
+++ PriceComparer/app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+// app/assets/javascripts/application.js
+//= require jquery
+//= require materialize
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+//= require materialize_init
