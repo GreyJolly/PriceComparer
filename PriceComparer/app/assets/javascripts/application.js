7948ae3ed8825ebe884195bbe2f6613f96dc31d9
frastricker <francesco.maura@engine4you.it>
2024-06-29 17:49:46 +0200

    hamburger menu on Home
    first implementation of Hamburger menu on index.html.erb

--- PriceComparer/app/assets/javascripts/application.js
+++ PriceComparer/app/assets/javascripts/application.js
@@ -1,7 +1,9 @@
 // app/assets/javascripts/application.js
-//= require jquery
-//= require jquery_ujs
+
+
 //= require materialize
 //= require rails-ujs
 //= require_tree .
 //= require materialize_init
+//= require navbar
+console.log("Application.js loaded");
---
+++ PriceComparer/app/assets/javascripts/navbar.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Navbar.js loaded");
+    var instances = M.Sidenav.init(elems, options);
+    var elems = document.querySelectorAll('.sidenav');
+  });
