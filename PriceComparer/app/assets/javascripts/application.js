1237693f0f7800b4633537fffd77def6c0a01ac6
frastricker <francesco.maura@engine4you.it>
2024-06-30 17:14:08 +0200

    made products dashboard and home responsive

--- PriceComparer/app/assets/javascripts/application.js
+++ PriceComparer/app/assets/javascripts/application.js
@@ -1,9 +1,9 @@
 // app/assets/javascripts/application.js
 
+//= require turbolinks
 
 //= require materialize
 //= require rails-ujs
 //= require_tree .
 //= require materialize_init
 //= require navbar
-console.log("Application.js loaded");
